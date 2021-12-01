87f77e04f54c81f88da8289b6721c2a9948a24dd
Paul Landaeta <paul.landeata@coxautoinc.com>
2021-12-01 19:49:41 -0400

    getPokemon

--- coding-1/pokecard/src/components/PokeCard.vue
+++ coding-1/pokecard/src/components/PokeCard.vue
@@ -2,16 +2,39 @@
 <div class="container">
   <b-form-input v-model="pokemonName" placeholder="Enter Pokemon name"></b-form-input>
   <b-button @click="getPokemon">Search</b-button>
-  <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+  <b-card no-body class="overflow-hidden">
     <b-row no-gutters>
       <b-col md="6">
         <b-card-img :src="imageUrl + pokemon.id + '.png'" alt="Image" class="rounded-0"></b-card-img>
       </b-col>
-      <b-col md="6">
-        <b-card-body title="Pikachu">
+      <b-col md="6" v-if="pokemon">
+        <b-card-body v-bind:title="pokemon.name">
           <b-card-text>
-            {{pokemonName}}This is a wider card with supporting text as a natural lead-in to additional content.
-            This content is a little bit longer.
+            <div class="d-flex justify-content-between">
+              <div>
+                <b>Type:</b>
+                <span v-for="(typeObject, index) in pokemon.types" :key="'value'+index">
+                  <span class="badge badge-primary">{{ typeObject.type.name}}</span>
+                </span>
+              </div>
+              </div>
+            <div class="d-flex justify-content-between">
+              <div>
+                <b>Height: </b>
+                <span>{{ pokemon.height }} m</span>
+              </div>
+              <div>
+                <b>Weight: </b>
+                <span>{{ pokemon.weight }} kg</span>
+              </div>
+            </div>
+            <!-- TODO: convert in a component -->
+            <b-container class="bv-example-row mb-3 text-left">
+              <b-row cols="2" v-for="(stat, index) in pokemon.stats" :key="'value'+index">
+                <b-col>{{ stat.stat.name }}</b-col>
+                <b-col>{{ stat.base_stat }}</b-col>
+              </b-row>
+            </b-container>
           </b-card-text>
         </b-card-body>
       </b-col>
@@ -32,7 +55,7 @@ export default {
       pokemons: [{
         id:''
       }],
-      pokemonName: '',
+      pokemonName: 'pikachu',
       pokemon: {
         id:'2',
       },
